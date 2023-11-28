94a11a87e3557493d195fc1aa9e1023bcbc30af3
John.Laspinas <baybayinFil@protonmail.com>
2023-11-28 14:03:57 +0800

    add: Add properties and protos message
    Change the adapter to preediting properties that
    are for preediting table.

--- NTDataHiveGrpcService/DAL/Model/PreEditingErrorFeedback.cs
+++ NTDataHiveGrpcService/DAL/Model/PreEditingErrorFeedback.cs
@@ -9,6 +9,7 @@
         public string PublisherName { get; set; }
         public string JournalId { get; set; }
         public string ArticleId { get; set; }
+        public string CopyEditedBy { get; set; }
         public int PageCount { get; set; }
         public int ErrorCount { get; set; }
         public string DescriptionOfError { get; set; }
@@ -21,6 +22,16 @@
         public string IntroducedOrMissed { get; set; }
         public string Department { get; set; }
         public string EmployeeName { get; set; }
+        public string RootCause { get; set; }
+        public string CorrectiveAction { get; set; }
+        public string NatureOfCA { get; set; }
+        public string OwnerOfCA { get; set; }
+        public DateTime TargetDateOfCompletionCA { get; set; }
+        public string PreventiveMeasure { get; set; }
+        public string NatureOfPM { get; set; }
+        public DateTime TargetDateOfCompletionPM { get; set; }
+        public string StatusOfCA { get; set; }
+        public string StatusOfPM { get; set; }
         public string CopyEditingLevel { get; set; }
         public DateTime CreatedAt { get; set; }
     }
