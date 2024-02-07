319a08991b1e49dfdcd644f3bcbeb7909ecc5099
John Jovence Laspinas <jlaspinas@novatechset.com>
2024-02-07 14:46:05 +0800

    feat: Feature to query feedback
    Query using the EmployeeName
    
    Getting all the neccessary needs to the
    request.

--- NTDataHiveGrpcService/DAL/GAP/PersistenceInterfaces/IEvaluationRecordPersistence.cs
+++ NTDataHiveGrpcService/DAL/GAP/PersistenceInterfaces/IEvaluationRecordPersistence.cs
@@ -4,7 +4,7 @@ namespace NTDataHiveGrpcService.DAL.GAP.PersistenceInterfaces
 {
     public interface IEvaluationRecordPersistence
     {
-        bool GetFeedBackByEmployeeName(string employeeName, out EvaluationFilter evaluationRecord);
+        List<FeedbackRecordRequest> GetFeedBackByEmployeeName(EvaluationFilter evaluationRecord);
         List<FeedbackRecordRequest> GetFeedbackRecords();
         bool Save(EvaluationFilter evaluationRecord);
         bool SelectById(string webId, out EvaluationFilter evaluationRecord);
