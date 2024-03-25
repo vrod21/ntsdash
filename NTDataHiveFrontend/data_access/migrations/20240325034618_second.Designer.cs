16774fdcc22b6033954c9dce7c319c09d1cc645b
Jess Belen <belenjaykin@gmail.com>
2024-03-25 11:50:12 +0800

    initial

--- NTDataHiveFrontend/data_access/migrations/20240325034618_second.Designer.cs
+++ NTDataHiveFrontend/data_access/migrations/20240325034618_second.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NTDataHiveFrontend.Data;
 
 #nullable disable
 
-namespace NTDataHiveFrontend.Migrations
+namespace NTDataHiveFrontend.data_access.migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240325034618_second")]
+    partial class second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
